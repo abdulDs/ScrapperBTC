fdb2fde36a315143a964d149a1d2239df791ceb7
abdulDs <59770855+abdulDs@users.noreply.github.com>
2020-11-22 21:28:14 +0100

    Update and rename mongodbA_Z.sh to redisinstall.sh

---
+++ mongodb/redisinstall.sh
@@ -0,0 +1,11 @@
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+
+sudo apt-get install tcl
+sudo apt-get install gcc
+sudo apt-get updatae
+cd redis-stable
+make
+cd src
+sudo apt-get install redis-server
+redis-server
